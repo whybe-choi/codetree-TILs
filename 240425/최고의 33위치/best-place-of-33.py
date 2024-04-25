6a7b9c050a9cb93ea079a8f56ff49208f9addd8c
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-04-25 23:43:45 +0900

    Title: 최고의 33위치, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240425/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,23 @@
+n = int(input())
+grid = []
+
+for _ in range(n):
+    row = list(map(int, input().split()))
+    grid.append(row)
+
+max_coin = 0
+
+for row in range(n):
+    for col in range(n):
+        if col + 2 >= n or row + 2 >= n:
+            continue
+
+        num_coin = 0
+
+        for r in range(row, row + 3):
+            for c in range(col, col + 3):
+                num_coin += grid[r][c]
+
+    max_coin = max(max_coin, num_coin)
+
+print(max_coin)
