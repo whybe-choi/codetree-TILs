f20615fa7b506b15cd7ed3c7058a4b851a66884b
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-04-29 12:35:24 +0900

    Title: 뿌요뿌요, Time: 171ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240429/뿌요뿌요/puyo-puyo.py
@@ -0,0 +1,37 @@
+n = int(input())
+
+board = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+
+def dfs(x, y, k):
+    global num_block
+    # 상, 하, 좌, 우
+    dxs, dys = [0, 0, -1, 1], [1, -1, 0, 0]
+    
+    for dx, dy in zip(dxs, dys):
+        new_x, new_y = x + dx, y + dy
+        if (new_x >= 0 and new_x < n) and (new_y >= 0 and new_y < n) and board[new_x][new_y] == k and not visited[new_x][new_y]:
+            visited[new_x][new_y] = True
+            num_block += 1
+            dfs(new_x, new_y, k)
+
+count = 0
+blocks = []
+
+for x in range(n):
+    for y in range(n):
+        # 같은 숫자로 이루어져 있는 경우 하나의 블럭으로 생각
+        k = board[x][y]
+        num_block = 0
+        if not visited[x][y]:
+            visited[x][y] = True
+            num_block += 1
+            dfs(x, y, k)
+            blocks.append(num_block)
+        
+        if num_block >= 4:
+            count += 1
+        
+max_block = max(blocks)
+
+print(count, max_block)
