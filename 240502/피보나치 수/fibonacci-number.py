1b0f859c48355421900651b92c6b76423d7c92bb
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-02 15:16:46 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240502/피보나치 수/fibonacci-number.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+dp = [-1 for _ in range(n)]
+
+# 초기값
+dp[0] = 1
+dp[1] = 1
+
+for i in range(2, n):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n-1])
