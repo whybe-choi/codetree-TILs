d95831b66a4ab162fbb68dfbefe927405528693e
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-02 15:55:41 +0900

    Title: 피보나치 수, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240502/피보나치 수/fibonacci-number.py
+++ 240502/피보나치 수/fibonacci-number.py
@@ -2,13 +2,13 @@ import sys
 
 n = int(sys.stdin.readline().rstrip())
 
-dp = [-1 for _ in range(n)]
+dp = [-1 for _ in range(45)]
 
 # 초기값
 dp[0] = 1
 dp[1] = 1
 
-for i in range(2, n):
+for i in range(2, 45):
     dp[i] = dp[i-1] + dp[i-2]
 
 print(dp[n-1])
