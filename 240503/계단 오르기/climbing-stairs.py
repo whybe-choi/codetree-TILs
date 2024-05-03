2356421637892a0953f9e27411fbe91a442e2bd4
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-03 14:35:09 +0900

    Title: 계단 오르기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240503/계단 오르기/climbing-stairs.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+dp = [0 for _ in range(1001)]
+
+dp[0] = 1
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, 1001):
+    dp[i] = dp[i-2] + dp[i-3]
+
+print(dp[n % 10007])
+print(dp)
