90d93277eb6c404b8834f10e41120f25e029b4c5
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-07 12:04:56 +0900

    Title: 연속되는 수 2, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240507/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+nums = []
+for _ in range(n):
+    num = int(sys.stdin.readline().rstrip())
+    nums.append(num)
+
+base = nums[0]
+count = 0
+for i in range(n):
+    if base == nums[i]: 
+        count += 1
+
+    base = nums[i]
+
+print(count)
