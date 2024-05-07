9b796ccb16840de463fc870b667d1464851570fb
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-07 11:44:44 +0900

    Title: 이상한 진수 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/이상한 진수 2/awkward-digits-2.py
+++ 240507/이상한 진수 2/awkward-digits-2.py
@@ -2,10 +2,10 @@ import sys
 
 a = sys.stdin.readline().rstrip()
 
-max_n = 0
+max_n = int(a, 2)
 for i in range(1, len(a)):
     if a[i] == "0":
         temp = a[:i] + "1" + a[i+1:]
-    max_n = max(max_n, int(temp, 2))
+        max_n = max(max_n, int(temp, 2))
 
 print(max_n)
