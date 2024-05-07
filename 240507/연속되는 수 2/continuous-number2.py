cdb77deec2169e9fb85ba4930661bfe6fbb8fd01
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-07 12:14:30 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240507/연속되는 수 2/continuous-number2.py
+++ 240507/연속되는 수 2/continuous-number2.py
@@ -1,21 +1,16 @@
 import sys
 
 n = int(sys.stdin.readline().rstrip())
+nums = [int(sys.stdin.readline().rstrip()) for _ in range(n)]
 
-nums = []
-for _ in range(n):
-    num = int(sys.stdin.readline().rstrip())
-    nums.append(num)
-
-base = nums[0]
-cnt = 1
-max_cnt = 1
-for i in range(1, n):
-    if base == nums[i]: 
+cnt = 0
+max_cnt = 0
+for i in range(n):
+    if i > 0 and arr[i] == arr[i-1]:
         cnt += 1
     else:
-        base = nums[i]
-        cnt = 1 
+        cnt = 1
+
     max_cnt = max(max_cnt, cnt)
 
 print(max_cnt)
