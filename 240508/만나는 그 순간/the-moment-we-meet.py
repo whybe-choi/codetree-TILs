09f2237c0b6a8e0a8b34feaae0bcda3e45ed1bcf
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-08 18:49:31 +0900

    Title: 만나는 그 순간, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240508/만나는 그 순간/the-moment-we-meet.py
+++ 240508/만나는 그 순간/the-moment-we-meet.py
@@ -7,13 +7,13 @@ a, b = [0], [0]
 for _ in range(n):
     d, t = sys.stdin.readline().rstrip().split()
     for _ in range(int(t)):
-        a.append(a[-1] + (1 if d == 'R' else -1)
+        a.append(a[-1] + (1 if d == 'R' else -1))
 
 # b
 for _ in range(m):
     d, t = sys.stdin.readline().rstrip().split()
     for _ in range(int(t)):
-        a.append(b[-1] + (1 if d == 'R' else -1)
+        a.append(b[-1] + (1 if d == 'R' else -1))
 
 min_len = min(len(a), len(b))
 
