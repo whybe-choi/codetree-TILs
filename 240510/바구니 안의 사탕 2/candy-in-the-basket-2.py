bd05ef1cd5a5dc2da881c486ffb71f7177d4a6be
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-10 14:12:14 +0900

    Title: 바구니 안의 사탕 2, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 240510/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240510/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -10,8 +10,11 @@ for _ in range(n):
     bucket[idx] += candy
 
 max_candy = 0
-for i in range(max_loc - 2*k + 1):
-    sum_candy = sum(bucket[i:i+2*k+1])
+for c in range(max_loc+1):
+    left = c-k if c-k > 0 else 0
+    right = c+k if c+k < max_loc else max_loc
+    sum_candy = sum(bucket[left:right+1])
     max_candy = max(max_candy, sum_candy)
+
 
 print(max_candy)
