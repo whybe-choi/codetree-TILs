0e3e39637ad20f9aadda70aeab0b478a1960251e
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-10 12:10:35 +0900

    Title: G or H 3, Time: 440ms, Memory: 28MB, Status: Passed - CodeTree

--- 240510/G or H 3/G-or-H-3.py
+++ 240510/G or H 3/G-or-H-3.py
@@ -9,7 +9,7 @@ for _ in range(n):
     score[int(loc)] = 1 if alpha == "G" else 2
 
 max_score = 0
-for i in range(1, 10001):
+for i in range(1, 10000-k+1):
     sum_score = sum(score[i:i+k+1])
     max_score = max(max_score, sum_score)
 
