46b31e0c098f647c2af2b61a1b70e440adba0476
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-10 12:08:28 +0900

    Title: G or H 3, Time: 226ms, Memory: 30MB, Status: Passed - CodeTree

--- 240510/G or H 3/G-or-H-3.py
+++ 240510/G or H 3/G-or-H-3.py
@@ -4,14 +4,12 @@ n, k = map(int, sys.stdin.readline().rstrip().split())
 
 score = [0 for _ in range(10001)]
 
-max_loc = 0
 for _ in range(n):
     loc, alpha = sys.stdin.readline().rstrip().split()
     score[int(loc)] = 1 if alpha == "G" else 2
-    max_loc = max(max_loc, int(loc))
 
 max_score = 0
-for i in range(1, max_loc-k+1):
+for i in range(1, 10001):
     sum_score = sum(score[i:i+k+1])
     max_score = max(max_score, sum_score)
 
