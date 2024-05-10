736e98a28503a46b2c1198c9ac8d0d3bed2ab8d0
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-10 12:40:07 +0900

    Title: 아름다운 수열 2, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240510/아름다운 수열 2/beautiful-sequence-2.py
+++ 240510/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 
 n, m = map(int, sys.stdin.readline().rstrip().split())
 a = list(map(int, sys.stdin.readline().rstrip().split()))
@@ -6,12 +7,7 @@ b = list(map(int, sys.stdin.readline().rstrip().split()))
 
 count = 0
 for i in range(n-m+1):
-    exists = True
-    for j in range(m):
-        if b[j] not in a[i:i+m]:
-            exists = False
-
-    if exists:
+    if Counter(a[i:i+m]) == Counter(b):
         count += 1
 
 print(count)
