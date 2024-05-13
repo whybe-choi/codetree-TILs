dca280c05fa80af1c71f81549aed6ca9ef68faa9
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-13 20:26:54 +0900

    Title: 트로미노, Time: 166ms, Memory: 29MB, Status: Passed - CodeTree

--- 240513/트로미노/tromino.py
+++ 240513/트로미노/tromino.py
@@ -14,7 +14,7 @@ dys = [[-1, 0], [1, 0], [0, 1], [0, -1], [0, 0], [-1, 1]]
 for x in range(n):
     for y in range(m):
         for dx, dy in zip(dxs, dys):
-            if (0 <= x+dx[0] < n) and (0 <= x+dx[1] < n) and (0 <= y+dy[0] < n) and (0 <= y+dy[1] < n):
+            if (0 <= x+dx[0] < n) and (0 <= x+dx[1] < n) and (0 <= y+dy[0] < m) and (0 <= y+dy[1] < m):
                 cases.append(grid[x][y] + grid[x+dx[0]][y+dy[0]] + grid[x+dx[1]][y+dy[1]])
 
 print(max(cases))
